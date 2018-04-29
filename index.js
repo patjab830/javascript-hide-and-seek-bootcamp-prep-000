27e7b0888c13a90dd6bd439de82d27094f74101d
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-29 03:25:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
+function increaseRankBy(n) {
+  var elRankedList = document.querySelectorAll(".ranked-list");
+  
+  for ( var i = 0; i < elRankedList.length; i++ ) {
+    for ( var j = 0; j < elRankedList[i].length; j++ ) {
+      elRankedList[i]++;
+    }
+  }
+}
+
+function()
