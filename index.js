f9c1696ef484bdb8ece5ea0d41005c9fd6f4c935
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-29 03:48:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var elRankedList = document.querySelectorAll("ul.ranked-list li");
+  
+  for ( var i = 0; i < elRankedList.length; i++ ) {
+    elRankedList[i].innerHTML = parseInt(elRankedList[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var nextEl = document.querySelector('div#grand-node');
+  
+  while ( nextEl.querySelector("div") !== null ) {
+    nextEl = nextEl.querySelector("div");
+  } 
+  
+  return nextEl;
+}
